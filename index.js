cdd8c679f151d56b3e63e8f364a842774be7abfa
Michael Baxter <baxt01@hotmail.com>
2024-02-27 13:52:26 +0000

    now I have changed from a simple inquirer prompt set to having a recursive loop determined by and bolean answer at the end as to weather there is to be another employee added once no is selected then the HTML is generated and save to the output DIR taking this aproach is in order to allow for multiple employees been added at the same time, I have also fixed the order in which the variables pulled from answers are sent through the classes and sub classes so that all the data is processed and out in the correct order giving acurate placement

--- index.js
+++ index.js
@@ -7,75 +7,108 @@ const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./src/page-template.js");
 
+const team = [];
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
-
-inquirer.prompt([
-    /* Pass your questions in here */
-    {
-    type: 'input',
-    name: 'name',
-    message: 'enter the employee name.',
-    default: 'Bob'
-    },
-    {
-        type: 'number',
-        name: 'id',
-        message: 'enter an ID for the employee.',
-        default: '1'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'enter the employee email address',
-        default: 'example@sample.com'
-    },
-    {
-        type: ' list',
-        name: 'role',
-        message: 'what role does the employee play in the company',
-        choices: ['Manager', 'Engineer', 'Intern'],
-        default: 'Manager'
-    },
-    {
-        type: 'number',
-        name: 'officeNumber',
-        message: ' enter your phone number',
-      when: function( answers ) {
-        return answers.role === 'Manager';
-      },
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'enter your github user name',
-        when: function( answers ) {
-            return answers.role === 'Engineer';
+// Function to prompt for employee details
+const promptEmployeeDetails = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter the employee name:',
+            default: 'Bob'
         },
-    },
-    {
-        type: 'input',
-        name: 'school',
-        message: 'what school is attended.',
-        when: function( answers ) {
-            return answers.role === 'Intern';
+        {
+            type: 'number',
+            name: 'id',
+            message: 'Enter an ID for the employee:',
+            default: '1'
         },
-    }
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-    console.log(answers);
-    let {name, id, email, role, officeNumber, github, school} = answers;
-    let employee;
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter the employee email address:',
+            default: 'example@sample.com'
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What role does the employee play in the company?',
+            choices: ['Manager', 'Engineer', 'Intern'],
+            default: 'Manager'
+        },
+        {
+            type: 'number',
+            name: 'officeNumber',
+            message: 'Enter the office number:',
+            when: function(answers) {
+                return answers.role === 'Manager';
+            },
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter the GitHub username:',
+            when: function(answers) {
+                return answers.role === 'Engineer';
+            },
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Enter the school attended:',
+            when: function(answers) {
+                return answers.role === 'Intern';
+            },
+        }
+    ]);
+};
 
-    if (role === 'Manager') {
-        employee = new Manager(name, id, email, role, officeNumber);
-    } else if (role === 'Engineer') {
-        employee = new Engineer(name, id, email, role, github);
-    } else if (role === 'Intern') {
-        employee = new Intern(name, id, email, role, school);
-    }
-  })
+// Recursive function to gather details for all roles
+const gatherEmployeeDetails = () => {
+    promptEmployeeDetails()
+        .then((answers) => {
+            let { name, id, email, role, officeNumber, github, school } = answers;
+            let employee;
+
+            if (role === 'Manager') {
+                employee = new Manager(name, id, email, role, officeNumber);
+            } else if (role === 'Engineer') {
+                employee = new Engineer(name, id, email, role, github);
+            } else if (role === 'Intern') {
+                employee = new Intern(name, id, email, role, school);
+            }
+
+            team.push(employee);
+
+            // Prompt for next employee or generate HTML if done
+            inquirer.prompt([
+                {
+                    type: 'confirm',
+                    name: 'addAnother',
+                    message: 'Do you want to add another employee?',
+                    default: false
+                }
+            ]).then((answer) => {
+                if (answer.addAnother) {
+                    gatherEmployeeDetails(); // Recursive call for next employee
+                } else {
+                    // Generate HTML for the entire team
+                    const html = render(team);
+
+                    // Save the HTML file
+                    fs.writeFileSync(outputPath, html);
+
+                    console.log('Team HTML file saved successfully!');
+                }
+            });
+        })
+        .catch((err) => {
+            console.error('Error occurred:', err);
+        });
+};
+
+// Start gathering employee details
+gatherEmployeeDetails();
